e2da51fb294c9c4ad415cbcf1e6d2781549b65eb
Sagn1k <mesagnik@yahoo.com>
2017-07-11 14:55:35 +0530

    updated cderds

--- creds.py
+++ creds.py
@@ -1,8 +1,8 @@
 #Alexa
-Security_Profile_Description =""
-Security_Profile_ID  = ""
-Client_ID = ""
-Client_Secret = ""
+Security_Profile_Description ="alexa web services"
+Security_Profile_ID  = "amzn1.application.1d2eda92bbaf4a6d8fefdbb34ff8822d"
+Client_ID = "amzn1.application-oa2-client.3bedce26cb044081b4eede9398e7294e"
+Client_Secret = "e8e34b220adee35adeff5189077e2b7cab3e46e5ba54c1cb3802a0793c3d75d7"
 Product_ID = ""
 
 #Redis
